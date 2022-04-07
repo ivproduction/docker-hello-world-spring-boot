eef3eda53252214014c5fd17539e83e39ec680d5
Your Name <you@example.com>
2022-04-07 14:15:58 +0000

    etst4

--- src/main/java/com/dockerforjavadevelopers/hello/Application.java
+++ src/main/java/com/dockerforjavadevelopers/hello/Application.java
@@ -14,6 +14,8 @@ public class Application {
     
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        int i = 0;
+        i++;
         
     }
 
